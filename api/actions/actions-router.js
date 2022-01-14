b89df2cac1779dabb40b063f7beb7dd251e94338
Jayson Byrd <jayson_byrd_95@outlook.com>
2022-01-14 05:24:43 -0500

    GET /:id functional

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -13,4 +13,19 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    Action.get(id)
+        .then(action => {
+            if(action){
+                res.status(200).json(action)
+            } else {
+                res.status(404).json({message: `Actions with id ${id} not found`})
+            }
+        })
+        .catch(() => {
+            res.status(500).json({message: "Error Retrieving Actions"})
+        })
+})
+
 module.exports = router

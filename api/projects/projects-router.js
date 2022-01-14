6c3a70383ce8f7c328b9f79f173c484b2ca9fb6a
Jayson Byrd <jayson_byrd_95@outlook.com>
2022-01-14 03:57:58 -0500

    GET /:id returns project if found, 404 if not

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -13,4 +13,20 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    Project.get(id)
+        .then(project => {
+            if(!project){
+                res.status(404).json({message: "Project not found!"})
+            } else {
+                res.status(200).json(project)
+            }
+        })
+        .catch(err => {
+            console.log(error)
+            res.status(500).json({message: "Error Fetching Project!"})
+        })
+})
+
 module.exports = router

9520203da93352f1b4edbb8ade6739eea1b1167c
Jayson Byrd <jayson_byrd_95@outlook.com>
2022-01-14 03:51:19 -0500

    GET / Returns all posts correctly

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,16 @@
-// Write your "projects" router here!
+const express = require('express')
+const Project = require('./projects-model')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    Project.get()
+        .then(projects => {
+            res.status(200).json(projects)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({message: "Error Fetching Projects!"})
+        })
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express')
 const PORT = process.env.PORT || 9000
 const server = require('./api/server')
 
+
+server.get('/', (req, res)=> {
+    res.send('<h2>Jaysons Sprint 1 API!</h2>')
+})
+
+
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })

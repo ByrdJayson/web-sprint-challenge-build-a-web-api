7f67100712ed1c92330052263f7be65078bfaaed
Jayson Byrd <jayson_byrd_95@outlook.com>
2022-01-14 05:05:47 -0500

    Projects Router Completed

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -24,7 +24,7 @@ router.get('/:id', (req, res) => {
             }
         })
         .catch(err => {
-            console.log(error)
+            console.log(err)
             res.status(500).json({ message: "Error Fetching Project!" })
         })
 })
@@ -44,43 +44,57 @@ router.post('/', (req, res) => {
                 res.status(500).json({ message: "Error Posting Project!" })
             })
     }
-
-    router.put('/:id', (req, res) => {
-        const { id } = req.params
-        const { name, description, completed } = req.body
-        if (!name || !description) {
-            res.status(400).json({ message: "Name and description are required" })
-        } else if (typeof completed === 'undefined') {
-            res.status(400).json({ message: "Completed is required" })
-        }
-        else {
-            Project.update(id, req.body)
-                .then(project => {
-                    res.status(200).json(project)
-                })
-                .catch(err => {
-                    console.log(err)
-                    res.status(500).json({ message: "Error Updating Project!" })
-                })
-        }
-    })
-
-    router.delete('/:id', (req, res) => {
-        const { id } = req.params
-        Project.remove(id)
-            .then(project => {
-                if(project) {
-                    res.status(200).json()
-                } else {
-                    res.status(404).json({message: `Project with id ${id} not found`})
-                }
-            })
-            .catch(() => {
-                res.status(500).json({message: `Error Deleting Project`})
-            })
-    })
-
-
 })
+router.put('/:id', (req, res) => {
+    const { id } = req.params
+    const { name, description, completed } = req.body
+    if (!name || !description) {
+        res.status(400).json({ message: "Name and description are required" })
+    } else if (typeof completed === 'undefined') {
+        res.status(400).json({ message: "Completed is required" })
+    }
+    else {
+        Project.update(id, req.body)
+            .then(project => {
+                res.status(200).json(project)
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(500).json({ message: "Error Updating Project!" })
+            })
+    }
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    Project.remove(id)
+        .then(project => {
+            if (project) {
+                res.status(200).json()
+            } else {
+                res.status(404).json({ message: `Project with id ${id} not found` })
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ message: `Error Deleting Project` })
+        })
+})
+
+router.get('/:id/actions', (req, res) => {
+    const { id } = req.params
+    Project.getProjectActions(id)
+        .then(actions => {
+            if(actions){
+                res.status(200).json(actions)
+            } else {
+                res.status(404).json({message: `Actions for project with id ${id} not found`})
+            }
+        })
+        .catch(() => {
+            res.status(500).json({message: "Error Fetching Project Actions"})
+        })
+})
+
+
 
 module.exports = router

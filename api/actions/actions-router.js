c1a31ab1fc5623b805d25a245fd43b0e597f21d0
Jayson Byrd <jayson_byrd_95@outlook.com>
2022-01-14 06:15:05 -0500

    stuck

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -52,4 +52,43 @@ router.post('/', (req, res) => {
     }
 })
 
+router.put('/:id'), (req, res) => {
+    const { id } = req.params
+    const { notes, project_id, description } = req.body
+    if(!project_id || !description || !notes) {
+        res.status(400).json({message: "Project ID, Description, and Notes are required"})
+    } else {
+        Project.get(project_id)
+            .then(project => {
+                if(project) {
+                    Action.update(project, req.body)
+                        .then(action => {
+                            res.status(201).json(action)
+                        })
+                        .catch(() => {
+                            res.status(500).json({message: "Error Updating Action"})
+                        })
+                } else {
+                    res.status(404).json({message: `Project ID ${project_id} not found`})
+                }
+            })
+    }
+}
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    Action.get(id)
+        .then(action => {
+            if(action) {
+                Action.remove(id)
+                    .then(() => {
+                        res.status(200)
+                    })
+            } else {
+                res.status(404).json({message: "Action Not Found"})
+            }
+        })
+
+})
+
 module.exports = router

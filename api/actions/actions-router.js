7802146d64b644b5064cb0a79498a7ec0902f1b6
Jayson Byrd <jayson_byrd_95@outlook.com>
2022-01-14 06:47:13 -0500

    stuck 2

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -55,38 +55,24 @@ router.post('/', (req, res) => {
 router.put('/:id'), (req, res) => {
     const { id } = req.params
     const { notes, project_id, description } = req.body
-    if(!project_id || !description || !notes) {
-        res.status(400).json({message: "Project ID, Description, and Notes are required"})
-    } else {
-        Project.get(project_id)
-            .then(project => {
-                if(project) {
-                    Action.update(project, req.body)
-                        .then(action => {
-                            res.status(201).json(action)
-                        })
-                        .catch(() => {
-                            res.status(500).json({message: "Error Updating Action"})
-                        })
-                } else {
-                    res.status(404).json({message: `Project ID ${project_id} not found`})
-                }
-            })
-    }
+    Action.update(id, req.body)
+        .then(action => {
+            res.status(200).json(action)
+        })
+        .catch(() => {
+            res.status(500).json({message: "Error Updating Action"})
+        })
 }
 
 router.delete('/:id', (req, res) => {
     const { id } = req.params
-    Action.get(id)
+    const { project_id, notes, description, completed} = req.body
+    Action.remove(id)
         .then(action => {
-            if(action) {
-                Action.remove(id)
-                    .then(() => {
-                        res.status(200)
-                    })
-            } else {
-                res.status(404).json({message: "Action Not Found"})
-            }
+            res.json(req.action)
+        })
+        .catch(() => {
+            res.status(500).json({message: "Error Removing Action"})
         })
 
 })
